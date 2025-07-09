e836126ddcb307c603a6879b8d5aa3cd28adae4f
Pdrzn <156123972+Pdrzn@users.noreply.github.com>
2025-07-09 17:11:28 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1,5 +1,6 @@
 # servidor.py
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,5 @@ def autorizados():
     return jsonify({"autorizados": IDS_AUTORIZADOS})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Render define essa porta
+    app.run(host="0.0.0.0", port=port)
